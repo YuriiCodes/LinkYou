6c3bb79ac84dba51cee02eaeab484b3e34f1217b
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2023-07-17 11:54:36 +0300

    add insta logo, update form size

--- src/routes/[username]/edit/+page.svelte
+++ src/routes/[username]/edit/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import { page } from "$app/stores";
-    import AuthCheck from "$lib/components/AuthCheck.svelte";
     import SortableList from "$lib/components/SortableList.svelte";
     import UserLink from "$lib/components/UserLink.svelte";
     import { db, userData, user } from "$lib/firebase";
@@ -19,6 +18,7 @@
       "TikTok",
       "LinkedIn",
       "GitHub",
+            "Instagram",
       "Custom",
     ];
     const formDefaults = {
@@ -79,7 +79,7 @@
     }
   </script>
   
-  <main class="max-w-xl mx-auto">
+  <main class="max-w-2xl mx-auto">
     {#if $userData?.username == $page.params.username}
       <h1 class="mx-2 text-2xl font-bold mt-8 mb-4 text-center">
         Edit your Profile
